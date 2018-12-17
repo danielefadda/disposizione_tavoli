921a4f2107d48b87e97a54be9b705052c60938a0
Unknown <fadda.daniele@gmail.com>
2018-12-17 13:50:48 +0100

    Cartella di progetto aggiornata.

--- js/tableau.js
+++ js/tableau.js
@@ -1,103 +1,115 @@
 console.log('ready');
 d3.csv('data/tableau.csv')
     .then(function (data) {
-        console.log('rows',data);
+        console.log('rows', data);
 
 
-        var table = d3.select('#tableau').append('table');
-        var thead = table.append('thead');
-        var tbody = table.append('tbody');
-
+        var table = d3.select('#tabellone').append('div')
+            .attr('class', 'container-fluid');
+        var thead = table.append('div')
+            .attr('class', 'thead');
+        var tbody = table.append('div')
+            .attr('class', 'tbody');
         // manipulate data
         var tavoliData = d3.nest()
             .key(d => d['nome tavolo'])
-            .rollup(function(leaves) {
-                    return {
-                        'elenco':leaves.map(d => d.invitato),
-                        'numero commensali':leaves.length,
-                    }
+            .rollup(function (leaves) {
+                return {
+                    'elenco': leaves.map(d => d.invitato),
+                    'numero commensali': leaves.length
+                }
             })
             .entries(data);
-        console.log('tavoliData',tavoliData);
+        console.log('tavoliData', tavoliData);
 
-        nomiTavoli = tavoliData.map(d=>d.key);
-        console.log('nomiTavoli',nomiTavoli);
+        nomiTavoli = tavoliData.map(d => d.key);
+        console.log('nomiTavoli', nomiTavoli);
 
-        datiTabella= tavoliData.map(function (d){
-                return {
-                    // 'numero invitati': +d.value['numero commensali'],
-                    'tavolo':d.key,
-                    'invitati':d.value.elenco,
-                    'lunghezzaElenco':+d.value.elenco.toString().length
-                }
+        datiTabella = tavoliData.map(function (d, i) {
+            return {
+                'numero invitati': +d.value['numero commensali'],
+                'N.': i + 1,
+                'Tavolo': d.key.toUpperCase(),
+                'Commensali': d.value.elenco.join(', ').toUpperCase(),
+                'lunghezzaElenco': +d.value.elenco.toString().length
+            }
         });
-        console.log('dataTable',datiTabella);
+        console.log('dataTable', datiTabella);
 
-        nomiTavoli = tavoliData.map(d=>d.key);
-        console.log('nomiTavoli',nomiTavoli);
+        nomiTavoli = tavoliData.map(d => d.key);
+        console.log('nomiTavoli', nomiTavoli);
 
-        var columns = ['tavolo','invitati']; //TODO:prenderli dai dati
+        var columns = ['N.', 'Tavolo', 'Commensali']; //TODO:prenderli dai dati
 
         // append the header row
-        thead.append('tr')
-            .selectAll('th')
+        thead.append('div')
+            .attr('class', 'row')
+            .selectAll('div')
             .data(columns)
             .enter()
-            .append('th')
-            .text(d => d)
-        ;
+            .append('div')
+            .attr('class', 'col')
+            .text(d => d.toUpperCase());
         // create a row for each object in the data
-        var rows = tbody.selectAll('tr')
+        var rows = tbody
+            .selectAll('div')
             .data(datiTabella)
             .enter()
-            .append('tr')
-        ;
+            .append('div')
+            .attr('class', 'row');
 
         // create a cell in each row for each column
-        var cells = rows.selectAll('td')
+        var cells = rows.selectAll('div')
             .data(function (row) {
                 return columns.map(function (column) {
-                    return {column: column, value: row[column]};
+                    return {
+                        column: column,
+                        value: row[column]
+                    };
                 });
             })
             .enter()
-            .append('td')
             .append('div')
-            // .attr('width', function (d,i) {
-            //     return d.value.map(d => d.length ).reduce((a, b) => a + b, 0)*13;
-            // })
-            .text(function (d) { return d.value; })
-        ;
+            .attr('class', 'col')
+            .append('span')
+            .attr('class', 'testo')
+            .text(function (d) {
+                return d.value;
+            });
 
-        // d3.selectAll('td:nth-child(1)')
-        //     .classed('numeroTavolo',true)
-        // ;
+        d3.selectAll('div.col:nth-child(1)')
+            .classed('col-1', true)
+            .classed('num-tavolo', true);
 
-        d3.selectAll('td:nth-child(1)')
-            .classed('nomeTavolo',true)
-        ;
-        lunghezza_elenco=datiTabella.map(d => d.lunghezzaElenco*17);
+        d3.selectAll('div.col:nth-child(2)')
+            .classed('col-3', true)
+            .classed('nomi-tavolo', true);
 
-        d3.selectAll('td:nth-child(2)')
-            .attr('class' , 'elenco')
-            .classed('marquee', function (d,i) {
-                return lunghezza_elenco[i] > 450;
-            })
-            .selectAll('div')
-            .attr('class' , 'testo')
-            .attr('width', function (d,i) {
-                return d.value.map(d => d.length ).reduce((a, b) => a + b, 0)*13;
-            })
-
-        ;
-
-
-
-            // https://www.jonathan-petitcolas.com/2013/05/06/simulate-marquee-tag-in-css-and-javascript.html
-        ;
-
-        console.log('lunghezza_elenco',lunghezza_elenco);
+        d3.selectAll('div.col:nth-child(3)')
+            .classed('col-8', true)
+            .classed('invitati', true);
+        //animate the string
+        d3.selectAll('div.tbody div.col:nth-child(1n)>span')
+            .each(function (d, i) {
+                if (getInnerWidth(d3.select(this).node()) > getInnerWidth(d3.select(this.parentNode).node())) {
+                    marquee(d3.select(this).node(), d3.select(this).node().innerText);
+                }
+            });
         return table;
     });
 
+getInnerWidth = function (elem) {
+    var style = window.getComputedStyle(elem);
+    return elem.offsetWidth - parseFloat(style.paddingLeft) - parseFloat(style.paddingRight) - parseFloat(style.borderLeftWidth) - parseFloat(style.borderRightWidth);
+};
 
+
+function marquee(htmlEl, text) {
+    text += '          ';
+    setInterval(function () {
+        let num = 1;
+        let result = text.substr(num) + text.substr(0, num);
+        htmlEl.innerText = result;
+        text = result;
+    }, 250);
+}
